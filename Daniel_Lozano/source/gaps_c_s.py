ce8c97cef638ed22511ca673189aca1595a32e96
daniel-lozano <d.lozano343@uniandes.edu.co>
2018-10-07 20:17:34 -0500

    prueba

--- Daniel_Lozano/source/gaps_c_s.py
+++ Daniel_Lozano/source/gaps_c_s.py
@@ -1,12 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-a=[1.5,2.0,2.5,3.]
-Ds=[0.1591,0.5353,1.273,2.125]
-Dc=[1.5061,1.5498,2.0919,2.861]
+a=[1.0,1.5,2.0,2.5,3.]
+Ds=[0.11039,0.1591,0.5353,1.273,2.125]
+Dc=[1.3515,1.5061,1.5498,2.0919,2.861]
 
-plt.plot(a,Ds,"o-",label=" $ \Delta_s  $")
-plt.plot(a,Dc,"^-",label="$ \Delta_c  $")
+plt.plot(a,Ds,"ks-",label=" $ \Delta_s  $")
+plt.plot(a,Dc,"ks--",alpha=0.3,label="$ \Delta_c  $")
 plt.legend()
 plt.grid()
 plt.xlim(0,3)
